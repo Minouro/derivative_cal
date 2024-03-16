6d0909ab0b411e05cd119d3930bb6265a02a50a2
Daniel Minoru <daniel.minoru.05@gmail.com>
2024-03-16 15:27:24 -0300

    Update With More Functions👌

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 import calculation.utils as utils
 
-m = utils.menu("\n1 - Inserir valores\n2 - Carregar planilha\nEscolha: ", ["1", "2"])
+m = 0
+while(int(m) != 8):
+    m = utils.menu("\n1 - Inserir valores\n2 - Erro Padrão\n3 - Derivada Parcial\n4 - Método dos Mínimos Padrões\n5 - Média e Desvio Padrão\n6 - Gráfico\n7 - Carregar planilha\n8 - Sair\n\nEscolha: ", ["1", "2","3","4","5","6","7","8"])
 
-if (int(m) == 1):
-    utils.newData()
-else:
-    utils.loadData()
+    if (int(m) == 1):
+        utils.newData()
+
+    elif(int(m) == 2):
+        utils.errorCal(utils.loadData())
+
+    elif(int(m) == 3):
+        utils.derivative(utils.loadData())
+
+    elif(int(m) == 4):
+        utils.minMethod(utils.loadData())
+
+    elif(int(m) == 5):
+        utils.Info(utils.loadData())
+    
+    elif(int(m) == 6):
+        utils.loadGraph(utils.loadData())
+
+    elif(int(m)) == 7:
+        utils.loadData()
